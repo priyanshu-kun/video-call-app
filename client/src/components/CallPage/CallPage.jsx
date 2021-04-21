6c9f07a16dcaabd3fa7e81c80ee74061d151008c
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2021-04-21 19:22:53 +0530

    A really buggy code

--- client/src/components/CallPage/CallPage.jsx
+++ client/src/components/CallPage/CallPage.jsx
@@ -5,37 +5,37 @@ import Messanger from "../HomePage/UI/Messanger/Messanger";
 import MessageListReducer from "./../../reducer/messageListReducer";
 import { getRequest, postRequest } from "./../../utils/apiRequest";
 import { baseURL, GET_CALL_ID, SAVE_CALL_ID } from "./../../utils/apiEndpoints";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 import "./CallPage.scss";
 import { useEffect, useState, useReducer } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import Peer from "simple-peer";
 
-const initialState = [];
-let peer = null;
-const socket = io.connect("http://localhost:5000");
+// const initialState = [];
+// let peer = null;
+const socket = io.connect('http://localhost:3000', { reconnect: true });
 
 function CallPage() {
-    let { id } = useParams();
-    // eslint-disable-next-line
-    const history = useHistory();
+    // let { id } = useParams();
+    // // eslint-disable-next-line
+    // const history = useHistory();
     const isAdmin = window.location.hash === "#init" ? true : false;
     const url = `${window.location.origin}${window.location.pathname}`;
     const [meetingInfoPopup, setMeetingInfoPopup] = useState(false);
     // eslint-disable-next-line
-    const [state, dispatch] = useReducer(MessageListReducer, initialState);
-    // eslint-disable-next-line
-    const [streamObj, setStreamObj] = useState();
-    // eslint-disable-next-line
-    const [screenCastStream, setScreenCastStream] = useState();
-    // eslint-disable-next-line
-    const [isPresenting, setIsPresenting] = useState(false);
-    // eslint-disable-next-line
-    const [isMessanger, setIsMessanger] = useState(false);
-    // eslint-disable-next-line
-    const [messageAlert, setMessageAlert] = useState({});
-    // eslint-disable-next-line
-    const [isAudio, setIsAudio] = useState(true);
+    // const [state, dispatch] = useReducer(MessageListReducer, initialState);
+    // // eslint-disable-next-line
+    // const [streamObj, setStreamObj] = useState();
+    // // eslint-disable-next-line
+    // const [screenCastStream, setScreenCastStream] = useState();
+    // // eslint-disable-next-line
+    // const [isPresenting, setIsPresenting] = useState(false);
+    // // eslint-disable-next-line
+    // const [isMessanger, setIsMessanger] = useState(false);
+    // // eslint-disable-next-line
+    // const [messageAlert, setMessageAlert] = useState({});
+    // // eslint-disable-next-line
+    // const [isAudio, setIsAudio] = useState(true);
 
 
     useEffect(() => {
@@ -43,70 +43,24 @@ function CallPage() {
             setMeetingInfoPopup(true);
         }
         initWebRTC();
-        socket.on("code", (data) => {
-            peer.signal(data)
-        })
+        // socket.on("code", (data) => {
+        //     peer.signal(data)
+        // })
         // eslint-disable-next-line
     }, [])
 
-    async function getRecieverCode() {
-        const response = await getRequest(`${baseURL}${GET_CALL_ID}/${id}`);
-        if (response.code) {
-            peer.signal(response.code);
-        }
-    }
+    // async function getRecieverCode() {
+    //     const response = await getRequest(`${baseURL}${GET_CALL_ID}/${id}`);
+    //     if (response.code) {
+    //         peer.signal(response.code);
+    //     }
+    // }
 
-    function initWebRTC() {
-        navigator.mediaDevices
-            .getUserMedia({
-                video: true,
-                audio: true,
-            })
-            .then((stream) => {
-                setStreamObj(stream);
-
-                peer = new Peer({
-                    initiator: isAdmin,
-                    trickle: false,
-                    stream: stream,
-                });
-
-                if (!isAdmin) {
-                    getRecieverCode();
-                }
-
-                peer.on("signal", async (data) => {
-                    if (isAdmin) {
-                        let payload = {
-                            id,
-                            signalData: data,
-                        };
-                        await postRequest(`${baseURL}${SAVE_CALL_ID}`, payload);
-                    } else {
-                        socket.emit("code", data, (cbData) => {
-                            console.log("code sent");
-                        });
-                    }
-                });
-                peer.on("connect", () => {
-                    console.log("Peer connected");
-                })
-
-                peer.on("stream", (stream) => {
-                    // got remote video stream, now let's show it in a video tag
-                    let video = document.querySelector("video");
-
-                    if ("srcObject" in video) {
-                        video.srcObject = stream;
-                    } else {
-                        video.src = window.URL.createObjectURL(stream); // for older browsers
-                    }
-
-                    video.play();
-                });
-            }).catch(e => {
-                console.log("ERROR: ", e);
-            })
+    const initWebRTC = () => {
+        // navigator.mediaDevices.getUserMedia({
+        //     video: true,
+        //     audio: true
+        // })
     }
 
     return (
--- server/server.js
+++ server/server.js
@@ -7,15 +7,11 @@ const server = http.createServer(app);
 
 app.use(cors(), express.json(), express.urlencoded({ extended: false }))
 app.use('/', require("./routes/routes"));
-const io = require("socket.io")(server, {
-    cors: {
-        origin: '*',
-    }
-});
+const io = (module.exports.io = require("socket.io")(server));
 const socketManager = require("./routes/socketManager");
 io.on("connection", socketManager);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`App is alive on http://localhost:${port}`);
 })
